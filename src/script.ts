71901a344efa8579fd51b15b72338c3b78dfff39
taro710 <taro710kata@gmail.com>
2024-03-13 00:32:02 +0900

    fix coffee table side

--- src/script.ts
+++ src/script.ts
@@ -97,11 +97,11 @@ const textureLoader = new THREE.TextureLoader(loadingManager);
 // /**
 //  * Environment map
 //  */
-const environmentMap = textureLoader.load("environment.jpg");
+const environmentMap = textureLoader.load("environment/environment.jpg");
 environmentMap.mapping = THREE.EquirectangularReflectionMapping;
 environmentMap.colorSpace = THREE.SRGBColorSpace;
 
-const environmentMap2 = textureLoader.load("environment2.jpg");
+const environmentMap2 = textureLoader.load("environment/environment2.jpg");
 environmentMap2.mapping = THREE.EquirectangularReflectionMapping;
 environmentMap2.colorSpace = THREE.SRGBColorSpace;
 
