87cbb51d3d951633b7d0fffd0fe1a79bd782890c
taro710 <katayamayutaro@team-lab.com>
2024-05-10 16:33:37 +0900

    bedroom build

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,13 @@ const root = resolve(__dirname, 'src');
 export default defineConfig({
   root,
   publicDir: '../static/',
+  base: './',
   server: {
     host: true, // Open to local network and display URL
     open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
   },
   build: {
     outDir: '../docs',
-    base: './',
     emptyOutDir: true, // Empty the folder first
     sourcemap: true, // Add sourcemap
     rollupOptions: {
