606ef2b045ceef0cd91e7e705e8eaf3e49abee73
taro710 <katayamayutaro@team-lab.com>
2024-05-10 16:44:07 +0900

    bedroom build

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const root = resolve(__dirname, 'src');
 export default defineConfig({
   root,
   publicDir: '../static/',
-  base: './',
+  base: '/threejs-my-house-interior/',
   server: {
     host: true, // Open to local network and display URL
     open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
